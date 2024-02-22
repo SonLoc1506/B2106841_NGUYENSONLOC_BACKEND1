6d2dc8a4e13730e60a6d890a01a2127fc83a8b60
SonLoc1506 <sonloc15.06.2k3@gmail.com>
2024-02-22 10:15:06 +0700

    Dinh nghĩa cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
