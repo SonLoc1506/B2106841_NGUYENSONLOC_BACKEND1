0e8209a8310bd067f864743b3d38a5765e3921c6
SonLoc1506 <sonloc15.06.2k3@gmail.com>
2024-02-22 12:11:42 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
-
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,16 @@ app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
+});
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({ 
+    message: err.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
